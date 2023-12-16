eeb918c40a608c74973ecef1e2c2728ead558c59
mohansai <mohansaikrishna.m20@iiits.in>
2023-12-16 15:16:55 +0530

    added routes and controllers

--- app.js
+++ app.js
@@ -1,41 +1,23 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors'); 
+const dotenv = require('dotenv');
+const wiki = require('./routes/Wiki');
+const translate = require('./routes/Translate');
+const bodyParser = require('body-parser');
+
+dotenv.config({path: 'config/config.env'})
 
 const app = express();
-const PORT = 5000;
-
 app.use(cors());
-
-app.get('/api/wiki/:query', async (req, res) => {
-  const query = req.params.     query;
-  try { 
-    const response = await axios.get(`https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&titles=${query}&formatversion=2&exsentences=10&exlimit=1&explaintext=1`);
-    const data = response.data;
-
-    res.json(data);
-  } catch (error) {
-    console.error('Error fetching Wikipedia article:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
-
-app.get('/api/related/:query', async (req, res) => {
-  const query = req.params.query;
-  try {
-    const response = await axios.get(`https://en.wikipedia.org/w/api.php?action=opensearch&search=${query}&format=json`);
-    const data = response.data;
-    res.json(data);
-  } catch (error) {
-    console.error('Error fetching Wikipedia article:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
+app.use(bodyParser.json());
+app.use('/api/wiki',wiki);
+app.use('/api/translator',translate);
 
 app.get('/', async (req, res) => {
-res.send('Hello World!');
+res.send('EaseWiki Server');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen(process.env.PORT,() =>{
+  console.log(`server started on port:${process.env.PORT}`)
+})
